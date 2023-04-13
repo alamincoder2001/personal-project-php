1dc421a003cb96e5bfda82d461146365a4331650
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-04-13 12:35:43 +0600

    update

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$emp = Employee::create();
-
-echo $emp;
+$employee = Employee::create();
+$setting = Student::read();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $setting; ?></title>
+</head>
+<body>
+    <div>
+        <?php echo $employee;?>
+    </div>
+</body>
+</html>
